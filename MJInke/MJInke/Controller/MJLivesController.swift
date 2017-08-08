20fbd7d2bff88a11b07277d3af5cf02d17fbcd28
Zakerberg <blmichael1129@icloud.com>
2017-08-08 22:13:41 +0800

    Just第三方库

--- MJInke/MJInke/Controller/MJLivesController.swift
+++ MJInke/MJInke/Controller/MJLivesController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class MJLivesController: UITableViewController {
 
+    let liveListUrl = "http://service.ingkee.com/api/live/gettop?imsi=&uid=17800399&proto=5&idfa=A1205EB8-0C9A-4131-A2A2-27B9A1E06622&lc=0000000000000026&cc=TG0001&imei=&sid=20i0a3GAvc8ykfClKMAen8WNeIBKrUwgdG9whVJ0ljXi1Af8hQci3&cv=IK3.1.00_Iphone&devi=bcb94097c7a3f3314be284c8a5be2aaeae66d6ab&conn=Wifi&ua=iPhone&idfv=DEBAD23B-7C6A-4251-B8AF-A95910B778B7&osversion=ios_9.300000&count=5&multiaddr=1"
+    
+    var list : [MJCell] = []
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Swift 映客直播"
