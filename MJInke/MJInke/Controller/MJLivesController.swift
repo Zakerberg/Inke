7e343ecef48b566a9b4066be74a6362eb91304b2
Zakerberg <blmichael1129@icloud.com>
2017-08-11 20:23:40 +0800

    Kingfisher ?

--- MJInke/MJInke/Controller/MJLivesController.swift
+++ MJInke/MJInke/Controller/MJLivesController.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
+import Kingfisher
+
 class MJLivesController: UITableViewController {
 
     let liveListUrl = "http://service.ingkee.com/api/live/gettop?imsi=&uid=17800399&proto=5&idfa=A1205EB8-0C9A-4131-A2A2-27B9A1E06622&lc=0000000000000026&cc=TG0001&imei=&sid=20i0a3GAvc8ykfClKMAen8WNeIBKrUwgdG9whVJ0ljXi1Af8hQci3&cv=IK3.1.00_Iphone&devi=bcb94097c7a3f3314be284c8a5be2aaeae66d6ab&conn=Wifi&ua=iPhone&idfv=DEBAD23B-7C6A-4251-B8AF-A95910B778B7&osversion=ios_9.300000&count=5&multiaddr=1"
     
     var list : [MJCell] = []
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,27 +32,39 @@ class MJLivesController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return list.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! MJLiveCell
 
-        // Configure the cell...
-
+        let live = list[indexPath.row]
+        
+        cell.nameLabel.text = live.nick
+        cell.adressLabel.text = live.location
+        cell.ViewersLabel.text = "\(live.views)"
+        
+        let imageUrl = URL(string:"http://img.meelive.cn/" + live.protrait)
+        
+//头像
+        
+        //cell.imgPor.kf
+        
+        
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
