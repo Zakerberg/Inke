02ca4583959019ebc2fe7f56d8f518aaddd85a70
Zakerberg <blmichael1129@icloud.com>
2017-08-12 09:57:02 +0800

    ...

--- MJInke/MJInke/Controller/MJLivesController.swift
+++ MJInke/MJInke/Controller/MJLivesController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import Just
+
 
 class MJLivesController: UITableViewController {
 
--- MJInke/Pods/Target Support Files/Pods-MJInke/Pods-MJInke-frameworks.sh
+++ MJInke/Pods/Target Support Files/Pods-MJInke/Pods-MJInke-frameworks.sh
@@ -82,3 +82,12 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Just/Just.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Kingfisher/Kingfisher.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Just/Just.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Kingfisher/Kingfisher.framework"
+fi
